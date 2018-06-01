27cac83d33b35c98f87bfa2c7c62fa1dac7ba297
EthChil <ethanchilderhose@gmail.com>
2018-06-01 09:50:14 -0400

    Finished MVG pipeline

--- RoamerPipeline.py
+++ RoamerPipeline.py
@@ -24,6 +24,7 @@ MVG_PATH = "~/openMVG_build/Linux-x86 64-RELEASE/"
 MVS_PATH = "~/openMVS_build/bin/"
 
 MVG_MATCHES = MVG_PATH + "matches"
+MVG_RECONSTRUCT = MVG_PATH + "reconstruction_sequential"
 
 #Get Directories
 INPUT_DIR = input("Enter the Input Directory")
@@ -53,6 +54,37 @@ os.rmdir(OUTPUT_DIR + "/LOGS")
 os.mkdir(OUTPUT_DIR + "/LOGS")
 prnt("Created LOGS Dir")
 
+#START OF MVG PIPELINE
 prnt("1. Intrinsics Analysis")
 pIntrinsics = subprocess.Popen([os.path.join(MVG_PATH, "openMVG_main_SfMInit_ImageListing"), "-i", INPUT_DIR, "-o", MVG_MATCHES, "-d", SENSOR_DATABASE_PATH])
 pIntrinsics.wait()
+
+prnt("2. Compute Features")
+pFeatures = subprocess.Popen([os.path.join(MVG_PATH, "openMVG_main_ComputeFeatures"),  "-i", MVG_MATCHES+"/sfm_data.json", "-o", MVG_MATCHES, "-m", "SIFT"])
+pFeatures.wait()
+
+prnt("3. Compute Matches")
+pMatches = subprocess.Popen( [os.path.join(MVG_PATH, "openMVG_main_ComputeMatches"),  "-i", MVG_MATCHES+"/sfm_data.json", "-o", MVG_MATCHES] )
+pMatches.wait()
+
+prnt ("4. Do Sequential/Incremental reconstruction")
+pRecons = subprocess.Popen( [os.path.join(MVG_PATH, "openMVG_main_IncrementalSfM"),  "-i", MVG_MATCHES+"/sfm_data.json", "-m", MVG_MATCHES, "-o", MVG_RECONSTRUCT] )
+pRecons.wait()
+
+prnt ("5. Colorize Structure")
+pRecons = subprocess.Popen( [os.path.join(MVG_PATH, "openMVG_main_ComputeSfM_DataColor"),  "-i", MVG_RECONSTRUCT + "/sfm_data.bin", "-o", MVG_RECONSTRUCT + "colorized.ply"] )
+pRecons.wait()
+
+# optional, compute final valid structure from the known camera poses
+print ("6. Structure from Known Poses (robust triangulation)")
+pRecons = subprocess.Popen( [os.path.join(MVG_PATH, "openMVG_main_ComputeStructureFromKnownPoses"),  "-i", MVG_RECONSTRUCT+"/sfm_data.bin", "-m", MVG_MATCHES, "-f", os.path.join(MVG_MATCHES, "matches.f.bin"), "-o", os.path.join(MVG_RECONSTRUCT,"robust.bin")])
+pRecons.wait()
+
+prnt("7. Recolorizing or smthng")
+pRecons = subprocess.Popen( [os.path.join(MVG_PATH, "openMVG_main_ComputeSfM_DataColor"),  "-i", MVG_RECONSTRUCT+"/robust.bin", "-o", os.path.join(MVG_RECONSTRUCT,"robust_colorized.ply")] )
+pRecons.wait()
+
+print ("7. Starting MVS conversion")
+pMVS = subprocess.Popen( [os.path.join(MVG_PATH, "openMVG_main_openMVG2openMVS"), "-i", MVG_RECONSTRUCT+"/sfm_data.bin", "-o", os.path.join(MVS_PATH, "output.mvs")] )
+pMVS.wait()
+#END OF MVG PIPELINE
